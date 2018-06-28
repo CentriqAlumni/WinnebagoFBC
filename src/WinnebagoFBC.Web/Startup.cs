db56ea32ed17f9358074d1bd3712a2d347a7400a
Jason Presley <jpresley@centriq.com>
2018-06-28 10:10:23 -0500

    Initial Commit

---
+++ src/WinnebagoFBC.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WinnebagoFBC.Web.Startup))]
+namespace WinnebagoFBC.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
